c045a13639735a37901cacd9f40b1645bbe1096c
Edward Leslie <edwardleslie09@gmail.com>
2018-10-30 20:28:30 -0400

    created AddFishForm component and added ability to update the state on App.js through the use of props

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { getFunName } from "../helpers.js";
 
 class StorePicker extends React.Component {
+  myinput = React.createRef();
+
+  goToStore = event => {
+    //1. Stop the form from submitting
+    event.preventDefault();
+    //2. Get the text from that input
+    const storeName = this.myinput.value.value;
+    //3. Change the page to /store/whatever they entered
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          ref={this.myinput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store</button>
       </form>
     );
