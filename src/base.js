b824917b41790e586c715aea839f35fd3fb9985b
Edward Leslie <edwardleslie09@gmail.com>
2018-10-31 12:59:54 -0400

    integrated firebase to enable persistant database. data is updating dynamically and the api keys have been moved to local .env file

---
+++ src/base.js
@@ -0,0 +1,17 @@
+import Rebase from "re-base";
+import firebase from "firebase";
+require("dotenv").config();
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL
+});
+
+const base = Rebase.createClass(firebaseApp.database());
+
+//this is a named export
+export { firebaseApp };
+
+//this is a default export
+export default base;
